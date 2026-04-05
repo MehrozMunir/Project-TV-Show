65f3dc5437358044927a87861b6f787722784b02
MehrozMunir <mehroz.pak@gmail.com>
2026-04-05 12:47:54 +0100

    level 100 completed

--- script.js
+++ script.js
@@ -6,7 +6,36 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
+
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+
+  const episodesGridContainer = document.querySelector(
+    ".episodes-grid-container",
+  );
+
+  episodeList.forEach((episode) => {
+    const episodeTemplate = document
+      .getElementById("episode-card-template")
+      .content.cloneNode(true);
+    const titleHeader = episodeTemplate.querySelector("h3");
+    const episodeImage = episodeTemplate.querySelector("img");
+    const episodeSummary = episodeTemplate.querySelector("p");
+    const episodeLink = episodeTemplate.querySelector("a");
+    const episodeCode =
+      episode.name +
+      " - S" +
+      episode.season.toString().padStart(2, "0") +
+      "E" +
+      episode.number.toString().padStart(2, "0");
+    titleHeader.textContent = episodeCode;
+    episodeImage.src = episode.image.medium;
+    episodeSummary.innerHTML = episode.summary;
+    episodeLink.href = episode.url;
+    episodeLink.textContent =
+      "Click here to visit the original source of this episode at TVMaze.com";
+
+    episodesGridContainer.appendChild(episodeTemplate);
+  });
 }
 
 window.onload = setup;
